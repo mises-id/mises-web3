1766068212228462fddc0454e85c3c424b514644
liskk <1056509662@qq.com>
2022-11-16 15:21:52 +0800

    feat: add theme

--- src/components/pageLoading/index.tsx
+++ src/components/pageLoading/index.tsx
@@ -2,7 +2,7 @@
 /*
  * @Author: lmk
  * @Date: 2022-05-25 23:57:44
- * @LastEditTime: 2022-09-14 16:39:52
+ * @LastEditTime: 2022-11-16 11:46:41
  * @LastEditors: lmk
  * @Description: 
  */
@@ -10,7 +10,7 @@ const Loading = () => {
   return <div className='flex items-center justify-center h-screen'>
     <div className="text-center">
       <img src="/logo192.png" alt="logo" width={80} height={80}/>
-      <p className="text-lg">Loading Web3 Sites...</p>
+      <p className="text-lg color-primary mt-10">Loading Web3 Sites...</p>
     </div>
   </div>
 }
